6ec37fc57915122c918d82ba835b8f72ace618e2
Qweeckusha <tolya.zaytsev.2004@gmail.com>
2024-09-01 01:40:03 +1100

    none cache

--- main.py
+++ main.py
@@ -14,10 +14,21 @@ templates = Jinja2Templates(directory='templates')
 
 Base.metadata.create_all(bind=engine)
 
+@app.middleware("http")
+async def add_cache_control_headers(request: Request, call_next):
+    response = await call_next(request)
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
 @app.get('/', response_class=HTMLResponse)
-async def read_root(request: Request, db: Session = Depends(get_db)):
-    users = db.query(User).all()
-    return templates.TemplateResponse('index.html', {"request": request, "users": users})
+async def read_root(request: Request):
+    return templates.TemplateResponse('index.html', {"request": request})
+
+@app.get('/about', response_class=HTMLResponse)
+async def about_page(request: Request):
+    return templates.TemplateResponse('about.html', {"request": request})
 
 
 @app.post("/add_user", response_class=HTMLResponse)
